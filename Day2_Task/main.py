91141ed84899c16da3a4bfc8ddab28ac80fa1402
ZainShakir <K190178@nu.edu.pk>
2022-06-21 18:11:45 +0500

    Python commit Zain

--- Day2_Task/main.py
+++ Day2_Task/main.py
@@ -1,7 +1,11 @@
+import math
+
+
 def PrimeCountingFunction():
     return "monday"
-def DistanceFormula():
-    return "tuesday"
+def DistanceFormula(x1,y1,x2,y2):
+    distance=math.sqrt(pow(x2-x1,2)+pow(y2-y1,2))
+    print(f"The distacne between the two points is {distance}")
 def QuadraticFormula():
     return "tuesday"
 def Slopeformula():
