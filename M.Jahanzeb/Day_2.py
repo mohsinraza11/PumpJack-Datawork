5323c7c02b581acebcdae907094836224087836e
Jahanzeb <jahanzebm201@gmail.com>
2022-06-21 18:32:00 +0500

    comm_6

--- M.Jahanzeb/Day_2.py
+++ M.Jahanzeb/Day_2.py
@@ -1,31 +1,81 @@
 import math
 
 def trig():
-    ch=input("Pick your choice\n1)Sin\n2)Cos\n3)Tan\n\n")
-    ch=int(ch)
-    val=input("Enter the number: ")
-    val=int(val)
+    
+    ch=input("Pick your choice\n1)Sin\n2)Cos\n3)Tan\n4)Exit\n\n")
 
-    match ch:
-        case 1:
-            print(math.sin(val))
-        case 2:
-            print(math.cos(val))
-        case 3:
-            print(math.tan(val))
-        case default:
-            print("Invalid input")
+    while True:
+        while True:
+          try:
+            ch = int(ch)
+            break
+          except ValueError:
+            print("No valid value! Please try again ...\n")
+            ch=input("")
+
+        if(ch==1):
+            while True:
+               try:
+                 val = input("Please enter a number: ")
+                 val = float(val)
+                 break
+               except ValueError:
+                print("No valid value! Please try again ...")
+            print("Sin: ",math.sin(val))
+            break
+
+        elif(ch==2):
+            while True:
+               try:
+                 val = input("Please enter a number: ")
+                 val = float(val)
+                 break
+               except ValueError:
+                print("No valid value! Please try again ...")
+            print("Cos: ",math.cos(val))
+            break
+        
+        elif(ch==3):
+            while True:
+               try:
+                 val = input("Please enter a number: ")
+                 val = float(val)
+                 break
+               except ValueError:
+                print("No valid value! Please try again ...")
+            print("Tan: ",math.tan(val))
+            break
+
+        elif(ch==4):
+            print("Good-bye!")
+            break
+
+        else:
+            print("Invalid input. Try again...\n")
+            ch=input("")
+
+
 
 
 def main():
 
-    val = input("Enter your choice\n1)Trignometry\n2)Exit\n\n")
-    val=int(val)
+    while True:
+        val = input("Please enter your choice\n1)Trignometry\n2)Exit\n ")
+        while True:
+          try:
+            val = float(val)
+            break
+          except ValueError:
+            print("No valid option! Please try again ...\n")
+            val = input("")
 
-    match val:
-        case 1:
+        if(val==1):
             trig()
-        case default:
-            print("Goodbye!")
-
+            break
+        elif (val==2):
+            print("E X I T")
+            break
+        else:
+            print("enter a valid option")
+        
 main()
