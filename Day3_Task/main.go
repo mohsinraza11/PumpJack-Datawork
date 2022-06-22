bce7604bf7cf2920bfd0c1a472640ae53ba8c108
Nehakhan5 <59708202+Nehakhan5@users.noreply.github.com>
2022-06-22 15:46:41 +0500

    Update main.go

--- Day3_Task/main.go
+++ Day3_Task/main.go
@@ -25,7 +25,32 @@ func CheckPrime() {
    fmt.Printf(" %d is a prime number\n", num)  
   }  
  }  
-}  
+}
+
+func quad(){
+var a, b, c, root1, root2, imaginary, discriminant float64
+
+    fmt.Print("Enter the a, b, c of Quadratic equation = ")
+    fmt.Scanln(&a, &b, &c)
+
+    discriminant = (b * b) - (4 * a * c)
+
+    if discriminant > 0 {
+        root1 = (-b + math.Sqrt(discriminant)/(2*a))
+        root2 = (-b - math.Sqrt(discriminant)/(2*a))
+        fmt.Println("Two Distinct Real Roots Exist: root1 = ", root1, " and root2 = ", root2)
+    } else if discriminant == 0 {
+        root1 = -b / (2 * a)
+        root2 = -b / (2 * a)
+        fmt.Println("Two Equal and Real Roots Exist: root1 = ", root1, " and root2 = ", root2)
+    } else if discriminant < 0 {
+        root1 = -b / (2 * a)
+        root2 = -b / (2 * a)
+        imaginary = math.Sqrt(-discriminant) / (2 * a)
+        fmt.Println("Two Distinct Complex Roots Exist: root1 = ", root1, "+", imaginary, " and root2 = ", root2, "-", imaginary)
+    }
+ }
+
 
 func slope(){
   var x1, x2, y1, y2 float64
@@ -71,7 +96,7 @@ case 1:
 case 2:
     fmt.Println("Tuesday")
 case 3:
-    fmt.Println("Tuesday")
+    quad()
 case 4:
     slope()
 case 5:
