df7a70b40fa0a55eaff6308280e08291e713dd8c
Nehakhan5 <59708202+Nehakhan5@users.noreply.github.com>
2022-06-22 15:42:17 +0500

    Update main.go

--- Day3_Task/main.go
+++ Day3_Task/main.go
@@ -32,6 +32,30 @@ func Sum() {
 
 	fmt.Println("The Sum of num1 and num2  = ", num1+num2)
 }
+func quad(){
+var a, b, c, root1, root2, imaginary, discriminant float64
+
+    fmt.Print("Enter the a, b, c of Quadratic equation = ")
+    fmt.Scanln(&a, &b, &c)
+
+    discriminant = (b * b) - (4 * a * c)
+
+    if discriminant > 0 {
+        root1 = (-b + math.Sqrt(discriminant)/(2*a))
+        root2 = (-b - math.Sqrt(discriminant)/(2*a))
+        fmt.Println("Two Distinct Real Roots Exist: root1 = ", root1, " and root2 = ", root2)
+    } else if discriminant == 0 {
+        root1 = -b / (2 * a)
+        root2 = -b / (2 * a)
+        fmt.Println("Two Equal and Real Roots Exist: root1 = ", root1, " and root2 = ", root2)
+    } else if discriminant < 0 {
+        root1 = -b / (2 * a)
+        root2 = -b / (2 * a)
+        imaginary = math.Sqrt(-discriminant) / (2 * a)
+        fmt.Println("Two Distinct Complex Roots Exist: root1 = ", root1, "+", imaginary, " and root2 = ", root2, "-", imaginary)
+    }
+ }
+
 
 var choice int
 //--------- my function --mustafain
@@ -48,7 +72,7 @@ case 1:
 case 2:
     fmt.Println("Tuesday")
 case 3:
-    fmt.Println("Tuesday")
+    quad()
 case 4:
     slope()
 case 5:
