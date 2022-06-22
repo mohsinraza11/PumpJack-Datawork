bc1df7d167c1f02eb5998a390af89f048db8471b
Mohsin Raza <91532536+mohsinraza11@users.noreply.github.com>
2022-06-22 15:37:48 +0500

    Update main.go

--- Day3_Task/main.go
+++ Day3_Task/main.go
@@ -4,23 +4,22 @@ import (
   "fmt"
 )
 
-func slope(){
-  var x1, x2, y1, y2 float64
-	fmt.Println("x1: ")
-	fmt.Scanln(&x1)
-	fmt.Println("x2: ")
-	fmt.Scanln(&x2)
-	fmt.Println("y1: ")
-	fmt.Scanln(&y1)
-	fmt.Println("y2: ")
-	fmt.Scanln(&y2)
-
-	m := (y2 - y1) / (x2 - x1)
-	fmt.Println("Slope = ", m)
-}
-
 var choice int
 //--------- my function --mustafain
+
+
+func Sum() {
+	var num1, num2 int
+	fmt.Print("Enter the First Number = ")
+	fmt.Scanln(&num1)
+	fmt.Print(" \n")
+
+	fmt.Print("Enter the Second Number = ")
+	fmt.Scanln(&num2)
+	fmt.Print(" \n")
+
+	fmt.Println("The Sum of num1 and num2  = ", num1+num2)
+}
 
 func main() {
 
@@ -36,13 +35,13 @@ case 2:
 case 3:
     fmt.Println("Tuesday")
 case 4:
-    slope()
+    fmt.Println("Thursday")
 case 5:
     fmt.Println("Friday")
 case 6:
     fmt.Println("Saturday")
 case 7:
-    fmt.Println("Sunday")
+  Sum()
 default:
     fmt.Println("Invalid")
 }
