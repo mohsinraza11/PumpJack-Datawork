2505d22d44a8c476e1fc0a58abfb38dd210b374c
MidhatKarim28 <102216708+MidhatKarim28@users.noreply.github.com>
2022-06-23 17:05:04 +0500

    Update Day3.go

--- MidhatKarim/Day3.go
+++ MidhatKarim/Day3.go
@@ -2,19 +2,18 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-func main() {
-	var x1, x2, y1, y2 float64
-	fmt.Println("x1: ")
-	fmt.Scanln(&x1)
-	fmt.Println("x2: ")
-	fmt.Scanln(&x2)
-	fmt.Println("y1: ")
-	fmt.Scanln(&y1)
-	fmt.Println("y2: ")
-	fmt.Scanln(&y2)
+func expand(x float64) float64 {
+	fmt.Println("(x-1)^3 = x**3 - 3*x**2 + 3*x - 1")
+	res1 := (math.Pow(x, 3))
+	res2 := -(3 * math.Pow(x, 2))
+	res3 := 3*x - 1
+	result := res1 + res2 + res3
+	return result
+}
 
-	m := (y2 - y1) / (x2 - x1)
-	fmt.Println("Slope = ", m)
+func main() {
+	fmt.Println(expand(4))
 }
